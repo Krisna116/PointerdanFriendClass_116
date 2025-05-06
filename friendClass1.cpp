cc40693e6ac2f97e1be8df2a6b14c4277fef84e8
Krisna <your-email@example.com>
2025-05-06 19:35:32 +0700

    1

---
+++ friendClass1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+public:
+    void setMana
+};
+
+orang::orang(/* args */)
+{
+}
+
+orang::~orang()
+{
+}
+
+{
+private:
+    /* data */
+public:
+    siswa;
+    class orang
+    {
+    private:
+        /* data */
+    public:
+        orang(/* args */);
+        ~orang();
+    };
+    
+    orang::orang(/* args */)
+    {
+    }
+    
+    orang::~orang()
+    {
+    }
+    (/* args */);
+    ~siswa;
+    class orang
+    {
+    private:
+        /* data */
+    public:
+        orang(/* args */);
+        ~orang();
+    };
+    
+    orang::orang(/* args */)
+    {
+    }
+    
+    orang::~orang()
+    {
+    }
+    ();
+};
+
+siswa;
+class orang
+{
+private:
+    /* data */
+public:
+    orang(/* args */);
+    ~orang();
+};
+
+orang::orang(/* args */)
+{
+}
+
+orang::~orang()
+{
+}
+::siswa;
+class orang
+{
+private:
+    /* data */
+public:
+    orang(/* args */);
+    ~orang();
+};
+
+orang::orang(/* args */)
+{
+}
+
+orang::~orang()
+{
+}
+(/* args */)
+{
+}
+
+siswa;
+class orang
+{
+private:
+    /* data */
+public:
+    orang(/* args */);
+    ~orang();
+};
+
+orang::orang(/* args */)
+{
+}
+
+orang::~orang()
+{
+}
+::~siswa;
+class orang
+{
+private:
+    /* data */
+public:
+    orang(/* args */);
+    ~orang();
+};
+
+orang::orang(/* args */)
+{
+}
+
+orang::~orang()
+{
+}
+()
+{
+}
